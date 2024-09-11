c2e53079d70d5a2ddf39a0f3909d0676384449ea
Mauricio Pisco <mauriciopisco@gmail.com>
2024-09-11 13:41:49 -0300

    Corregir typo en funcion para apikey

--- netlify/functions/key.mjs
+++ netlify/functions/key.mjs
@@ -1,7 +1,7 @@
 export default async (req, context) => {
 
     const REFERER = req.headers.get("Referer");
-    if (REFERER = context.site.url) {
+    if (REFERER === context.site.url) {
         return new Response(Netlify.env.get("OPEN_WEATHER_KEY"));
     } else {
         return new Response(null, {status: 401});
