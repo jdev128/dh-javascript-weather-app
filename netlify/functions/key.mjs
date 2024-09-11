3c102b2606b4313e1425205f993489bad0b9d1e4
Mauricio Pisco <mauriciopisco@gmail.com>
2024-09-11 14:40:21 -0300

    Corregir error en funcion netlify

--- netlify/functions/key.mjs
+++ netlify/functions/key.mjs
@@ -2,7 +2,7 @@ export default async (req, context) => {
 
     const REFERER = req.headers.get("Referer");
     console.log(REFERER, context.site.url);
-    if (REFERER.includes(context.site.url)) {
+    if (REFERER?.includes(context.site.url)) {
         return new Response(Netlify.env.get("OPEN_WEATHER_KEY"));
     } else {
         return new Response(null, {status: 401});
