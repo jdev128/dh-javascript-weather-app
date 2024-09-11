24408f72d27b60b4a08130f13a445cf25bb4998d
Mauricio Pisco <mauriciopisco@gmail.com>
2024-09-11 13:19:50 -0300

    Ajustar trazabilidad en funciones Netlify

--- netlify/functions/key.mjs
+++ netlify/functions/key.mjs
@@ -1,7 +1,7 @@
 export default async (req, context) => {
 
-    console.info("Referer: ", req.headers.get("Referer"));
-    console.info("Origin: ", req.headers.get("Origin"));
+    console.log("Referer: ", req.headers.get("Referer"));
+    console.log("Origin: ", req.headers.get("Origin"));
 
 	return new Response(Netlify.env.get("OPEN_WEATHER_KEY"));
 };
